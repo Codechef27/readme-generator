aa2472ca59b664a670540985af2d705f751e88a8
BradleyK <bradleyjkennett@gmail.com>
2022-06-13 23:49:03 -0400

    badge add

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -4,7 +4,7 @@ function renderLicenseBadge(license) {
   if(license === 'other' || 'none') {
     return "";
   }else {
-    return `[![Badge](https://img.shields.io/badge-${license}&success)]`
+    return `[![Badge](https://img.shields.io/badge/license-${license}-success)]`
   }
 }
 
@@ -43,70 +43,67 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-
-  if(license === 'MIT') {
-    return `[license](https://choosealicense.com/licenses/mit/)`;
-  }
-  if(license === 'APACHE 2.0'){
-    return `[license](https://choosealicense.com/licenses/apache-2.0)`;
-  }
-  if(license === 'GNU GPL 3.0') {
-    return `[license](https://choosealicense.com/licenses/gpl-3.0/)`;
-  }
-  if(license === 'ISC') {
-    return `[license](https://choosealicense.com/licenses/isc/)`;
-  }
-  else if (license === 'none') {
-    return 'No License';
-  }
-  else {
-    return "";
+  if (!license) {
+    return '';
+  } else if (license) {
+    return `## License 
+    
+    Licensed by ${renderLicenseLink(license)}`;
   }
 
-}
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
+
+  ${renderLicenseBadge(data.license)}
   
   # ${data.title}
 
-  ## License 
 
-  ${renderLicenseBadge(data.license)}
-  ${renderLicenseLink(data.license)}
-  
+  ${renderLicenseSection(data.license)}
+
   ## Table of Contents
 
-  [Description](#description),
-  [Installation](#installation),
-  [Languages](#languages),
-  [Usage](#usage),
-  [Contribution](#contribution)
-  [Test](#test))
-  [Questions](#questions),
+  - [Description](#description),
+  - [Installation](#installation),
+  - [Languages](#languages),
+  - [Usage](#usage),
+  - [Contribution](#contribution)
+  - [Test](#test))
+  - [Questions](#questions),
 
   ## Description
+
   ${data.description}
 
   ## Languages Used
+
   ${data.languages}
 
   ## Usage 
+
   ${data.usage}
 
-  ##Install Instructions
+  ## Install Instructions
+
   ${data.installation}
 
   ## Tests
+
   ${data.test}
 
   ## Contributing
+
   ${data.contribution}
 
   ## Questions
-  Github Account:[${data.github}](https://github.com/${data.github})
-  Email Me: ${data.email}
+
+  Contact Me!
+
+  * Github Account:[${data.github}](https://github.com/${data.github})
+  * Email Me: ${data.email}
 `;
 }
 
